a1ef2dd9a4732e4f80d50b8d4d29867211f3b1d8
falla055 <145510014+falla055@users.noreply.github.com>
2023-10-22 23:07:13 -0400

    Lab3code
    Pushing the files of lab3; Conception d'une calculatrice simple avec Android Studio

---
+++ lab3/Calculatrice3/app/src/main/java/com/example/calculatrice/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.calculatrice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private enum Operator{none, add, minus, multiply, divide}; //enum of all different operands
+    private double data1 = 0, data2 = 0;
+    private Operator optr =Operator.none; //instanciating an operator of value none to start
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btn00Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"0");
+    }
+    public void btn01Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"1");
+    }
+    public void btn02Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"2");
+    }
+    public void btn03Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"3");
+    }
+    public void btn04Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"4");
+    }
+    public void btn05Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"5");
+    }
+    public void btn06Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"6");
+    }
+    public void btn07Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"7");
+    }
+    public void btn08Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"8");
+    }
+    public void btn09Click(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+"9");
+    }
+    public void btnAddClick(View view){
+        optr = Operator.add;
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        data1 = Double.parseDouble(eText.getText().toString());
+        eText.setText(""); //add button doesn't need to display any text
+    }
+    public void btnMinusClick(View view){
+        optr = Operator.minus;
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        data1 = Double.parseDouble(eText.getText().toString());
+        eText.setText(""); //add button doesn't need to display any text
+    }
+    public void btnMultiplyClick(View view){
+        optr = Operator.multiply;
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        data1 = Double.parseDouble(eText.getText().toString());
+        eText.setText(""); //add button doesn't need to display any text
+    }
+    public void btnDivideClick(View view){
+        optr = Operator.divide;
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        data1 = Double.parseDouble(eText.getText().toString());
+        eText.setText(""); //add button doesn't need to display any text
+    }
+    public void btnClearClick(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText("");
+    }
+    public void btnDotClick(View view){
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+".");
+    }
+    public void btnResultClick(View view){
+        if(optr != Operator.none){
+            EditText eText = (EditText)findViewById(R.id.resultEdit);
+            data2 = Double.parseDouble(eText.getText().toString());
+            double result = 0;
+            if(optr == Operator.add){
+                result = data1+data2;
+            } else if (optr == Operator.minus){
+                result = data1-data2;
+            } else if (optr == Operator.multiply){
+                result = data1*data2;
+            } else if (optr == Operator.divide){
+                result = data1/data2;
+            }
+            //reset the operator and display the result
+            optr = Operator.none;
+            data1 = result;
+            if((result-(int)result)!=0)
+                eText.setText(String.valueOf(result));
+            else
+                eText.setText(String.valueOf((int)result));
+        }
+    }
+
+
+}
